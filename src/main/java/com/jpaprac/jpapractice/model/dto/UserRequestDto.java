a248ac57144a65f60a9d281e6145263ce8cd9de3
Gayeon <henzel1013@gmail.com>
2025-01-16 09:13:18 +0900

    reqeust,response dto 분리

---
+++ src/main/java/com/jpaprac/jpapractice/model/dto/UserRequestDto.java
@@ -0,0 +1,11 @@
+package com.jpaprac.jpapractice.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRequestDto {
+    private String name;
+    private String email;
+
+    // Getters and Setters
+}
---
+++ src/main/java/com/jpaprac/jpapractice/model/dto/UserResponseDto.java
@@ -0,0 +1,12 @@
+package com.jpaprac.jpapractice.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String email;
+
+    // Getters and Setters
+}
